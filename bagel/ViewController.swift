f322128dc51bae07a5878e774b5f2974d52940fa
alex <tte577es7@yandex.ru>
2022-02-16 11:07:31 +0300

    Initial Commit

---
+++ bagel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bagel
+//
+//  Created by Александр on 16/02/2022.
+//  Copyright © 2022 Александр. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
