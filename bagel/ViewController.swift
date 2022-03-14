7d400f4f1fc67fa8ea1b55f427678dbb21bef9d2
Александр <32023344+AlexandrRumiantsev@users.noreply.github.com>
2022-03-14 16:21:48 +0300

    ап бублик
    Сделал возможность поворачивать бублик на 45 градусов бесконечно

--- bagel/ViewController.swift
+++ bagel/ViewController.swift
@@ -16,36 +16,42 @@ class ViewController: UIViewController {
     
     @objc func innerСircleClicked(_ sender: UITapGestureRecognizer) {
         
-        let alert = UIAlertController(title: "Выберете вариант взаимодействия с окружностью", message: "Доступны действия: изменить размер и перемещение, вращение на 45 градусов.", preferredStyle: .alert)
+        let alert = UIAlertController(
+            title: "Выберете вариант взаимодействия с окружностью",
+            message: "Доступны действия: изменить размер и перемещение, вращение на 45 градусов.",
+            preferredStyle: .alert
+        )
          
         alert.addAction(UIAlertAction(title: "изменить размер и переместить", style: .default, handler: { action in
             
             UIView.animate(withDuration: 0.5, delay: 0.4, options: [], animations: {
-            
                 
-                self.bubbleView.bounds = CGRect(x: 10, y: 0, width: 200, height: 200)
-                self.bubbleView.shapeLayer.bounds = CGRect(x: -10, y: 0, width: 200, height: 200)
-  
+                self.bubbleView.bounds = CGRect(x: 10, y: 20, width: 200, height: 200)
+                self.bubbleView.shapeLayer.bounds = CGRect(x: -10, y: -20, width: 200, height: 200)
+      
             }, completion: nil)
             
         }))
         
         
         alert.addAction(UIAlertAction(title: "повернуть на 45 градусов", style: .default, handler: { action in
+            
             UIView.animate(withDuration: 1, delay: 0.4, options: [], animations: {
-               
-                self.bubbleView.transform = CGAffineTransform(rotationAngle: CGFloat(Double(-45) * .pi/180))
+                
+                var radians = atan2f(Float(self.bubbleView.transform.b), Float(self.bubbleView.transform.a));
+                var degrees = radians * (180 / Float(M_PI));
 
+                self.bubbleView.transform = CGAffineTransform(rotationAngle: CGFloat(Double(Double(degrees-45) * .pi/180)))
+                
                 
             }, completion: { action in
                 print(self.bubbleView.frame)
                 print(self.bubbleView.bounds)
             })
+            
         }))
          
         self.present(alert, animated: true)
-        
-        
         
     }
     
